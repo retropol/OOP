f5472c49a424641a443b944b5a63556da4d28a76
retropol <mehmet.demirturk@ogr.sakarya.edu.tr>
2024-09-29 22:19:54 +0300

    edit

--- Animals/Abstract/Animals.cs
+++ Animals/Abstract/Animals.cs
@@ -11,30 +11,5 @@ namespace OOP.Animals.Abstract
 
 
 
-    class Program
-    {
 
-        static void Main(string[] args)
-        {
-            Kartal kartal = new Kartal();
-            kartal.Name = "Kaya";
-            kartal.Age = "10";
-            kartal.MakeNoise();
-            kartal.Fly();
-
-            Fish fish = new Fish();
-            fish.Name = "Flipper";
-            fish.Age = "15";
-            fish.MakeNoise();
-            fish.Swim();
-
-            Lion lion = new Lion();
-            lion.Name = "Simba";
-            lion.Age = "55";
-            lion.MakeNoise();
-            lion.Run();
-        }
-
-
-    }
 }
---
+++ Worlds/Main/Main.cs
@@ -0,0 +1,31 @@
+﻿using OOP.Animals.Concrete;
+
+namespace OOP.Worlds.Main
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Kartal kartal = new Kartal();
+            kartal.Name = "Kaya";
+            kartal.Age = "10";
+            kartal.MakeNoise();
+            kartal.Fly();
+
+            Fish fish = new Fish();
+            fish.Name = "Flipper";
+            fish.Age = "15";
+            fish.MakeNoise();
+            fish.Swim();
+
+            Lion lion = new Lion();
+            lion.Name = "Simba";
+            lion.Age = "55";
+            lion.MakeNoise();
+            lion.Run();
+        }
+
+
+    }
+}
